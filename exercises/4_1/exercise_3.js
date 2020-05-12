0c5852a6bbe8c31f4b09c5c1518a5802e8f5b335
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-05-12 18:28:53 -0300

    Fiz o exercício 4.

---
+++ exercises/4_1/exercise_3.js
@@ -0,0 +1,10 @@
+let a = 936473, b = 936473, c = 996473;
+if (a > b && a > c) {
+  console.log("a = " + a);
+} else if (b > a && b > c) {
+  console.log("b = " + b);
+} else if (c > a && c > b) {
+  console.log("c = " + c);
+} else {
+  console.log("Os números são iguais!");
+}
