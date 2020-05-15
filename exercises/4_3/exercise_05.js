b22a05077fdde4c9890d152e9a577f331d81da19
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-05-15 17:56:12 -0300

    Enviei a resolução do 4 e 5 da aula ao vivo.

--- exercises/4_3/exercise_05.js
+++ exercises/4_3/exercise_05.js
@@ -7,3 +7,71 @@ n = 7
   * *
  *   *
 ******* */
+// let n = 9;
+// let numberOfLines = (n + 1) / 2; // 5
+// let controlLeft = numberOfLines; // 5
+// let controlRight = numberOfLines; // 5
+// let lineInput = "";
+
+/*
+
+    *    
+   * *  
+  *   *
+ *     *
+*********
+*/
+
+// for (let i = 0; i < numberOfLines; i+=1) {
+//   for (let lineColumn = 1; lineColumn <= n; lineColumn+=1) {
+//     if (lineColumn <= controlLeft && lineColumn >= controlRight) {
+//       lineInput = lineInput + "*";
+//     } else {
+//       lineInput = lineInput + " ";
+//     }
+//   }
+//   console.log(lineInput);
+//   lineInput = "";
+
+//   controlRight -= 1; // 5 - 4 - 3 - 2 - 1
+//   controlLeft += 1; // 5 - 6 - 7 - 8 - 9
+// }
+
+
+//---------------------
+
+let n = 9;
+let numberOfLines = (n + 1) / 2; // 5
+let controlLeft = numberOfLines; // 5
+let controlRight = numberOfLines; // 5
+let lineInput = "";
+
+/*
+
+    *    
+   * *  
+  *   *
+ *     *
+*********
+*/
+
+for (let i = 0; i < numberOfLines; i+=1) {
+  for (let lineColumn = 1; lineColumn <= n; lineColumn+=1) {
+    // Se é a última linha, preenche com * em tudo
+    if (i === (numberOfLines - 1)) {
+      lineInput = lineInput + "*";
+    // Para todas as outras linhas, utilizar as variáveis de
+    } else if (lineColumn === controlLeft || lineColumn === controlRight) {
+      lineInput = lineInput + "*";
+    } else {
+      lineInput = lineInput + " ";
+    }
+  }
+
+  console.log(lineInput);
+  lineInput = "";
+
+  controlRight -= 1; // 5 - 4 - 3 - 2 - 1
+  controlLeft += 1; // 5 - 6 - 7 - 8 - 9
+}
+
