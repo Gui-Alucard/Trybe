d0c935068f749c0621787a15eba5b512134b146d
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-06-24 00:18:07 -0300

    Fiz o ex002, não da para usar o toContain pq a function já faz oq ele praticamente retornaria. toBe retorna o valor que a função quer.

--- exercises/9_1/ex002.js
+++ exercises/9_1/ex002.js
@@ -1,5 +1,3 @@
-const assert = require('assert');
-
 function myIndexOf(arr, item) {
   let searchedIndex = -1;
   for (let i = 0; i < arr.length; i += 1) {
@@ -10,11 +8,4 @@ function myIndexOf(arr, item) {
   return searchedIndex;
 }
 
-// implemente seus testes aqui
-/*
-2. A função myIndexOf(arr, item) recebe um array arr, um item e retorna o índice
-do item ou -1 caso o item não pertença ao array arr
-
-Teste se a chamada myIndexOf([1, 2, 3, 4], 3) retorna o valor esperado
-Teste se a chamada myIndexOf([1, 2, 3, 4], 5) retorna o valor esperado
-*/
+module.exports = myIndexOf;
