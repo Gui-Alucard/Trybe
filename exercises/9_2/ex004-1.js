8984c59e2b2b952d723fa46f43d3bb9caf7cefa0
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-06-25 11:42:11 -0300

    Ao chamar o callbak, percebi que o param temperature não estava sendo retornado (NaN ou Undefined), dai entendi que o parametro do callback deveria ser o RESULTADO da function getMarsTemperature que foi chamada como função no paran line 17.

--- exercises/9_2/ex004-1.js
+++ exercises/9_2/ex004-1.js
@@ -10,7 +10,12 @@ const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 // definição da função sendMarsTemperature...
-
+const sendMarsTemperature = (getMarsTemperature, (callback) => {
+  setTimeout(() => {
+    console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`)
+    callback(getMarsTemperature());
+  }, messageDelay());
+});
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
