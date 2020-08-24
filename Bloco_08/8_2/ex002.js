fd51756f3e538fa16aa70463da1133e66e955de2
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-08-24 12:31:50 -0300

    ex002/8-2 done with map & sort

--- Bloco_08/8_2/ex002.js
+++ Bloco_08/8_2/ex002.js
@@ -63,9 +63,52 @@ const books = [
   },
 ];
 
-// Crie uma string com os nomes de todas as pessoas autoras. use reduce
-function allNames() {
-  // escreva seu código aqui
+const expected_result = [
+  {
+    age: 31,
+    author: 'Isaac Asimov'
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft'
+  },
+  {
+    age: 39,
+    author: 'Stephen King'
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin'
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert'
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien'
+  }
+];
+
+/*
+Construa um array de objetos a partir do array de livros.
+Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro,
+e uma propriedade age com a idade dessa pessoa quando o livro foi lançado.
+O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha
+considerando suas idades quando o livro foi lançado. Use MAP e SORT.
+*/
+function nameAndAge() {
+  const objectBook = books.map(book => {
+    const authorAge = book.releaseYear - book.author.birthYear;
+    const authorName = book.author.name;
+
+    return {
+      age: authorAge,
+      author: authorName
+    }
+  })
+  return objectBook.sort((count, current) => count.age - current.age);
 }
 
-assert.deepEqual(allNames(), "Nomes: George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.");
+
+assert.deepEqual(nameAndAge(), expected_result);
