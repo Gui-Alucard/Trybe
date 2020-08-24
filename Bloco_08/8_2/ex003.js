134ef8ad540152647b7fd136db4042ba817efdd7
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-08-24 13:49:22 -0300

    ex003/8-2 done with filter

--- Bloco_08/8_2/ex003.js
+++ Bloco_08/8_2/ex003.js
@@ -64,14 +64,51 @@ const books = [
 ];
 
 const expected_result = [
-  'O Senhor dos Anéis',
-  'Fundação',
-  'O Chamado de Cthulhu'
+ {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
 ]
 
-// Crie um array com o nome de todos os livros com mais de 60 anos de publicação. use as funções filter e map
-function oldBooks() {
-  // escreva seu código aqui
+// Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+function fantasyOrScienceFiction() {
+  return books.filter(book => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
 }
 
-assert.deepEqual(oldBooks(), expected_result);
+assert.deepEqual(fantasyOrScienceFiction(), expected_result);
